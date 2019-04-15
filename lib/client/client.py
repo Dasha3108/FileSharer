7d88e8dd4b802f2d8bc6d0629485cee1fc30f9ae
KseniyaBorokhovskaya <kseniya.borokhovskaya@gmail.com>
2019-04-15 21:13:20 +0300

    New logic for connection

--- lib/client/client.py
+++ lib/client/client.py
@@ -16,6 +16,28 @@ class Client:
     def connect_to_server(self):
         self.socket.connect((self.server_ip, self.server_port))
 
+    def bind(self):
+        self.socket.bind((self.server_ip, self.server_port))
+
+    def run(self):
+        self.bind()
+        self.receive_file()
+
+    def upload_file(self, file_name):
+        self.connect_to_server()
+        #connection, address = self.socket.accept()
+        self.send_file(file_name)
+
+    def send_file(self, file_name):
+        file = open(file_name, 'rb')
+
+        file_data = file.read(1024)
+
+        # TODO: encrypt data
+
+        self.socket.send(file_data)
+        self.socket.close()
+
     def receive_file(self):
         """
             Receives the file from server and saves it to the temp file
@@ -52,6 +74,4 @@ class Client:
 
         file.close()
 
-    def run(self):
-        self.connect_to_server()
-        self.receive_file()
+
--- lib/server/server.py
+++ lib/server/server.py
@@ -1,5 +1,7 @@
 import socket
 from threading import Thread
+import lib.client.utils as utils
+from lib.client.constants import TEMP_FILE_NAME
 
 
 class Server:
@@ -11,9 +13,11 @@ class Server:
 
         self.socket = socket.socket()
 
+    def bind_to_port(self):
+        self.socket.bind((self.server_ip, self.server_port))
 
     def connect_to_port(self):
-        self.socket.bind((self.server_ip, self.server_port))
+        self.socket.connect((self.server_ip, self.server_port))
 
     def disconnect(self, connection):
         connection.close()
@@ -23,9 +27,21 @@ class Server:
 
     def connect(self):
         connection, address = self.socket.accept()
-        t = Thread(target=self.send_file, args=[connection, 'file.txt'])
-        t.start()
+        data = connection.recv(1024)
+        if data != '':
+            t = Thread(target=self.receive_file, args=[data])
+            t.start()
+        else:
+            t = Thread(target=self.send_file, args=[connection, 'file.txt'])
+            t.start()
         #return connection, address
+
+
+    def run(self, number_of_clients):
+        self.bind_to_port()
+        self.listen(number_of_clients)
+        #connection, address =
+        self.connect()
 
     def send_file(self, connection, file_name):
 
@@ -37,8 +53,35 @@ class Server:
 
         connection.send(file_data)
 
-    def run(self, number_of_clients):
-        self.connect_to_port()
-        self.listen(number_of_clients)
-        #connection, address =
-        self.connect()
+    def receive_file(self, data):
+
+        #file = utils.create_file(TEMP_FILE_NAME)
+
+        temp_file = open(TEMP_FILE_NAME, 'wb')
+
+        encrypted_data = data   #self.socket.recv(1024)
+        self.socket.close()
+
+        # TODO: decrypt the received data
+        temp_file.write(encrypted_data)
+
+        self.temp_file = temp_file
+
+        temp_file.close()
+
+        self.save_received_file('file1.txt')
+
+    def save_received_file(self, file_name):
+
+        file = utils.create_file(file_name)
+
+        if self.temp_file is None:
+            return
+
+        file_data = utils.get_data_from_file(self.temp_file.name)
+
+        file.write(file_data)
+
+        file.close()
+
+
